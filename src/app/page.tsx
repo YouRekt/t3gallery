298610523b2854623f2945509b314850d66551a7
YouRekt <czarnecki.jurko@gmail.com>
2024-08-21 22:43:07 +0200

    chore: Update homepage to dynamically fetch and display images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,42 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-  console.log(images);
+async function Images() {
+	const images = await db.query.images.findMany({
+		orderBy: (model, { desc }) => desc(model.id),
+	});
 
-  return (
-    <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            {/* <Image
+	return (
+		<div className="flex flex-wrap gap-4">
+			{[...images, ...images, ...images].map((image, index) => (
+				<div key={image.id + "-" + index} className="flex w-48 flex-col">
+					{/* <Image
               src={image.url}
               width={1920}
               height={1080}
               alt={`Image ${image.id}`}
             /> */}
-            <img src={image.url} alt={`Image ${image.id}`} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+					<img src={image.url} alt={`Image ${image.id}`} />
+					<div>{image.name}</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default function HomePage() {
+	return (
+		<main>
+			<SignedOut>
+				<div className="h-full w-full text-2xl text-center">
+					Please Sign In to see the gallery
+				</div>
+			</SignedOut>
+			<SignedIn>
+				<Images />
+			</SignedIn>
+		</main>
+	);
 }
