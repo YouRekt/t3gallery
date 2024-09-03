e7bbe246054853ee7c9f9d100eae8a4d2b0789d2
YouRekt <czarnecki.jurko@gmail.com>
2024-09-03 21:53:17 +0200

    chore: Update modal component and full image page layout

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,5 +3,18 @@ import { getImage } from "~/server/db/queries";
 export default async function FullPageImageView(props: { id: number }) {
 	const image = await getImage(props.id);
 
-	return <img src={image.url} className="w-96" />;
+	return (
+		<div className="flex w-full h-full min-w-0">
+			<div className="flex-shrink flex justify-center items-center">
+				<img
+					src={image.url}
+					className="flex-shrink object-contain max-h-full max-w-full"
+				/>
+			</div>
+
+			<div className="w-48 flex flex-col flex-shrink-0 border-l">
+				<div className="text-xl font-bold">{image.name}</div>
+			</div>
+		</div>
+	);
 }
